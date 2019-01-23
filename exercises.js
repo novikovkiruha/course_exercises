e80e12307af2441cf43c2e7ddea3729844d68fbf
novikovkiruha <novikov.k.ru@gmail.com>
2019-01-23 21:31:50 +0200

    release v1

---
+++ exercises.js
@@ -0,0 +1,97 @@
+
+//1 task________________________________________________________________
+// var enteredNumber = prompt ("Enter a number");
+
+// if (enteredNumber % 2 === 0) alert("Even number");
+// else alert("Odd number");
+
+
+//2 task________________________________________________________________
+// var number1 = prompt ("Enter the 1st number");
+// var number2 = prompt ("Enter the 2nd number");
+// var number3 = prompt ("Enter the 3rd number");
+
+// var numbersArray = [number1, number2, number3];
+
+// var maxValue;
+// var minValue;
+// for (var i = 0; i < numbersArray.length; i++) {
+	// maxValue = numbersArray[0];
+	// minValue = numbersArray[0];
+	// if (numbersArray[i] > maxValue) {
+		// maxValue = numbersArray[i];
+	// } else {
+		// minValue = numbersArray[i];
+	// }
+// }
+
+// alert(maxValue + " is a highest number and " + minValue + " is a lowest number");
+
+
+//3 task________________________________________________________________
+// var daysNumber = prompt("Enter a number of days");
+
+// var week = Math.floor(daysNumber / 7);
+// var days = daysNumber % 7;
+
+// if (week > 1) {
+	// if (days > 1) {
+		// alert(week + " weeks and " + days + " days");
+	// } else if (days === 0) {
+		// alert(week + " weeks");
+	// } else {
+		// alert(week + " weeks and " + days + " day");
+	// }
+// }
+// else if (week === 1) {
+	// if (days > 1) {
+		// alert(week + " week and " + days + " days");
+	// } else if (days === 0) {
+		// alert(week + " week");
+	// } else {
+		// alert(week + " week and " + days + " day");
+	// }
+// }
+// else if (week === 0) {
+	// if (days > 1) {
+		// alert(days + " days");
+	// } else if (days === 1) {
+		// alert(days + " day");
+	// } else {
+		// alert("You entered '0'");
+	// }
+// }
+
+
+//4 task________________________________________________________________
+
+
+
+//5 task________________________________________________________________
+
+
+
+//6 task________________________________________________________________
+
+
+
+
+
+//7 task________________________________________________________________
+
+
+
+
+
+//8 task________________________________________________________________
+
+
+
+
+
+//9 task________________________________________________________________
+
+
+
+
+//10 task________________________________________________________________
